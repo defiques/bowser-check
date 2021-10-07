3faf94e631414b448658ab5ab397faf8447f2bf9
Alexander <72750745+defiques@users.noreply.github.com>
2021-10-07 13:01:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const result = bowser.getParser(window.navigator.userAgent);
 console.log(result.getBrowser());
 
 const validBrowser = result.satisfies({
+    chrome: '<94.0.4606.71',
     firefox: '>5.0',
     safari: '>6.0.4',
     'internet explorer': '>10',
